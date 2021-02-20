bc714f967f0abdbe208901549da05de43f29c521
juanluna96 <juandluna17@gmail.com>
2021-02-20 14:23:17 -0500

    Styled Components para el Contenido Principal

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from '@emotion/styled';
+import imagen from './cryptomonedas.png';
+
+const Contenedor = styled.div`
+  max-width:900px;
+  margin:0 auto;
+  @media (min-width: 992px) {
+    display:grid;
+    grid-template-columns:repeat(2,1fr);
+    column-gap:2rem;
+  }
+`;
+
+const Imagen = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+  filter: grayscale(75%);
+`;
+
+const Heading = styled.h1`
+  font-family: 'Bebas Neue', cursive;
+  color:#fff;
+  text-align:700;
+  font-weight:700;
+  font-size:50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+
+  &::after{
+    content:'';
+    width:250px;
+    height:6px;
+    background-color: #a3a261;
+    display: block;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <div className="">
+        <Imagen src={ imagen } alt="imagen crypto"></Imagen>
+      </div>
+      <div className="">
+        <Heading>Cotiza criptomonedas al instante</Heading>
+      </div>
+    </Contenedor>
   );
 }
 
