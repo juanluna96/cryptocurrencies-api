c9e8d403cf92e5db6d01d46e0d314cd230fa2c4a
juanluna96 <juandluna17@gmail.com>
2021-02-20 16:14:30 -0500

    Creando el Componente Formulario

---
+++ src/components/Formulario.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Boton = styled.button`
+  margin-top: 20px;
+  font-weight: bold;
+  font-size:20px;
+  padding: 10px;
+  background-color: #CAB797;
+  border:none;
+  width: 100%;
+  border-radius: 10px;
+  color: #FFFFFF;
+  transition: background-color 0.5s ease;
+
+  &:hover{
+    background-color: #978a72;
+    cursor: pointer;
+  }
+`;
+
+const Formulario = () => {
+    return (
+        <form action="">
+            <Boton type="submit">CALCULAR</Boton>
+        </form>
+    )
+}
+
+export default Formulario
